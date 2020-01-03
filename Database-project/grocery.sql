a0a1522403209dcd000cd5d0b199ef34a1cca7eb
Maneesh <memaneesh@hotmail.com>
2020-01-03 16:06:03 +0530

    New product database sql server

---
+++ Database-project/grocery.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[grocery]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NULL, 
+    [price] INT NULL, 
+    [code] VARCHAR(50) NULL, 
+    [created] DATETIME NULL
+)
